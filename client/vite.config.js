293f88159577103db6a5ec79e38bfe2bf7eedab9
sagar1952115 <sagarkumar1952115@gmail.com>
2024-03-04 20:50:34 +0530

    modified

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [, react()],
-  base:"https://khata.onrender.com/api/auth/login"
+  plugins: [react()],
 })
